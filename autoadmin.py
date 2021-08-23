d46d9d1e2c18a2b385d91388285f1c49428258e6
Marcin Swierczewski <>
2021-08-23 01:26:40 +0100

    creating admin file

--- autoadmin.py
+++ autoadmin.py
@@ -1,2 +1,12 @@
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neural_network.settings")
+
+import django
+django.setup()
+
+from django.core.management import call_command
+
 from django.contrib.auth.models import User
+
 User.objects.create_superuser('admin', 'admin@example.com', 'pass')
