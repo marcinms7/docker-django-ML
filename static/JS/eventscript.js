4eed468a50ebdb46a196f48c88fc941469791807
Marcin Swierczewski <>
2021-09-04 17:05:37 +0100

    implemented loader

--- static/JS/eventscript.js
+++ static/JS/eventscript.js
@@ -1,14 +1,31 @@
-var headOne = document.querySelector('#one')
 
-headOne.addEventListener('mouseover', function(){
-  headOne.textContent = 'MAKE THIS ANIMATED!'
-}
-)
 
-headOne.addEventListener('mouseout', function(){
-  headOne.textContent = 'LOADING MODEL '
+var myVar;
+myVar = setTimeout(showPage, 3000);
+
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+
+  
+	var headOne = document.querySelector('#one')
+
+	headOne.addEventListener('mouseover', function(){
+	  headOne.textContent = 'MAKE THIS ANIMATED!'
+	}
+	)
+
+	headOne.addEventListener('mouseout', function(){
+	  headOne.textContent = 'LOADING MODEL '
+	}
+	)
+
+
+
 }
-)
+
+
 
 
 
